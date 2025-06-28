4afc9c0b61190cad60d9b7f7034d757afc130434
Luc Oostdijk <27276350+Lo-o@users.noreply.github.com>
2025-06-28 22:28:34 +0200

    fs proj

---
+++ Bots/LucBot/EastBankFsTrader.fs
@@ -0,0 +1,20 @@
+﻿module EastBankFsTrader
+
+open System.Threading.Tasks
+open NasdaqTrader.Bot.Core
+
+type MyTraderBot() =
+
+    // Define a private field or property for the company name
+    let companyName = "FSharp Traders Inc."
+
+    interface ITraderBot with
+
+        member _.DoTurn(systemContext: ITraderSystemContext) : Task =
+            // Implement your async logic here
+            task {
+                // Do something with systemContext
+                return ()
+            } :> Task
+
+        member _.CompanyName = companyName
---
+++ Bots/LucBot/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace LucBot
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
